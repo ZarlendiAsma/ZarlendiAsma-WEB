f3d837be12c13c62587329d6bf5d0ec19de73847
ZarlendiAsma <muhammadzrlndiasma@gmail.com>
2023-04-04 15:32:50 +0700

    WEB

---
+++ OOP/constanst.php
@@ -0,0 +1,16 @@
+<?php 
+
+// define('NAMA', 'Zarlendi Asma');
+// echo NAMA;
+
+// echo '<br>';
+
+// const NIP = 1234;
+// echo NIP;
+
+class Coba {
+    // define('NAMA', 'Zarlendi Asma');
+    const NAMA = 'Zarlendi Asma';
+}
+
+echo Coba::NAMA;
